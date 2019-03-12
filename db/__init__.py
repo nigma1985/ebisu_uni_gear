166e77f9a9931538072a143831d92c58f88ac275
nigma1985 <nigma-mania@web.de>
2019-03-12 22:46:26 +0100

    implement database OOP

--- db/__init__.py
+++ db/__init__.py
@@ -9,3 +9,31 @@ results = cur.fetchall()
 
 for result in results:
     print(result)
+
+class database:
+
+    ## Class Attribute
+    # species = 'mammal'
+    # year = 2019
+
+    # Initializer / Instance Attributes
+    def __init__(self, db_type, host, user, password, dbname):
+        self.conn = ''
+        if host is not None:
+            self.conn = self.conn + ' host=' + host
+        if user is not None:
+            self.conn = self.conn + ' user=' + user
+        if password is not None:
+            self.conn = self.conn + ' password=' + password
+        if dbname is not None:
+            self.conn = self.conn + ' dbname=' + dbname
+
+        self.conn = psycopg2.connect(self.conn)
+        self.cur = self.conn.cursor()
+
+        self.cur.execute('select * from people')
+        self.results = self.cur.fetchall()
+
+# Instantiate the Dog object
+philo = database(db_type=None, host='copyright', user='pi', password='21255Dohren', dbname='test')
+print(philo.results)
