5c2278534182145bbe51b21a255629c10a1aef35
nigma1985 <nigma-mania@web.de>
2019-03-19 18:21:26 +0100

    introducing postgresql

--- __init__.py
+++ __init__.py
@@ -10,12 +10,22 @@ os.chdir("../ebisu_uni_gear/")
 activities = json2py(jsonPath = 'input/activities_20151126.json')
 ebisu = database(db_type=None, host='copyright', user='pi', password='21255Dohren', dbname='test')
 
-d2s.dict2sql(
-    dictionary = activities[0],
-    tableName = 'moves_activities',
-    database = ebisu,
-    addNames = ['user'],
-    addValues = ['konrad.keck@live.de'])
+# d2s.dict2sql(
+#     dictionary = activities[0],
+#     tableName = 'moves_activities',
+#     database = ebisu,
+#     addNames = ['user'],
+#     addValues = ['konrad.keck@live.de'])
+x = ebisu.results
+
+print(
+    type(x),
+    x)
+
+ebisu.createTable('test')
+
+# for result in ebisu.results:
+#     print(result)
 
 # print(len(activities))
 # print(activities[0])
