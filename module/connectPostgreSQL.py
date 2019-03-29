ee18c21a9d396c9769042e9b06c63d7553adbf15
nigma1985 <nigma-mania@web.de>
2019-03-30 00:51:51 +0100

    get Query

--- module/connectPostgreSQL.py
+++ module/connectPostgreSQL.py
@@ -64,6 +64,119 @@ class database:
 
         return returnValue
 
+    def getQuery(
+        query = None, option = '',
+        schema_name = 'public', table_name = None,
+        listNames = [], listValues = [],
+        whereNames = [], whereValues = []):
+
+        ## check variables
+        if query is None:
+            raise Exception('missing query')
+        if not isinstance(query, str):
+            raise Exception('query is type {}. Only string is allowed.'.format(type(query)))
+        if table_name is None:
+            raise Exception('missing table name')
+        if not isinstance(table_name, str):
+            raise Exception('table name is type {}. Only string is allowed.'.format(type(table_name)))
+        if (listNames is None) or (len(listNames) == 0):
+            raise Exception('missing list of names')
+        if (listValues is None) or (len(listValues) == 0):
+            raise Exception('missing list of values')
+        if not isinstance(listNames, (list, tuple)):
+            raise Exception('names are type {}. Only lists or tuples allowed.'.format(type(listNames)))
+        if not isinstance(listValues, (list, tuple)):
+            raise Exception('values are type {}. Only lists or tuples allowed.'.format(type(listValues)))
+        if len(listNames) != len(listValues):
+            raise Exception('unequal number of names ({}) and values ({})'.format(len(listNames),len(listValues)))
+
+        result = None
+        table = table_name.lower()
+        if query = 'get ID':
+            result = []
+            if option is None:
+                raise Exception('missing option')
+            elif not isinstance(option, str):
+                raise Exception('option is type {}. Only string allowed.'.format(type(option)))
+            elif option is 'strict':
+                for n in range( len(listNames) ):
+                    result.append('(\"{}\" = \'{}\')'.format(listNames[n], listValues[n]))
+            elif option is 'include NULL':
+                for n in range( len(listNames) ):
+                    result.append('((\"{}\" = \'{}\') OR (\"{}\" IS NULL))'.format(listNames[n], listValues[n]), listNames[n])
+            else:
+                raise Exception('unknown option: \'{}\''.format(option))
+            result = '''SELECT MAX(id)
+                FROM \"''' + table + '''\"
+                WHERE ''' + '''
+                AND '''.join(result) + '''
+                ;'''
+            return result
+
+        elif query = 'create table':
+            result = '''CREATE TABLE IF NOT EXISTS \"{}\"
+                (id SERIAL PRIMARY KEY);'''.format(table)
+            return result
+
+        elif query = 'add columns':
+            result = []
+            for n in range( len(listNames) ):
+                result.append('ADD COLUMN IF NOT EXISTS \"{}\" TEXT'.format(listNames[n]))
+            result = '''ALTER TABLE IF EXISTS {}
+                '''.format(table) + ''',
+                '''.join(result) + '''
+                ;'''
+            return result
+
+        elif query = 'update set':
+            if (whereNames is None) or (len(whereNames) == 0):
+                raise Exception('missing list of names')
+            if (whereValues is None) or (len(whereValues) == 0):
+                raise Exception('missing list of values')
+            if not isinstance(whereNames, (list, tuple)):
+                raise Exception('names are type {}. Only lists or tuples allowed.'.format(type(whereNames)))
+            if not isinstance(whereValues, (list, tuple)):
+                raise Exception('values are type {}. Only lists or tuples allowed.'.format(type(whereValues)))
+            if len(whereNames) != len(whereValues):
+                raise Exception('unequal number of names ({}) and values ({})'.format(len(whereNames),len(whereValues)))
+
+            result = []
+            where = []
+            for n in range( len(listNames) ):
+                result.append('(\"{}\" = \'{}\')'.format(listNames[n], listValues[n]))
+            for n in range( len(whereNames) ):
+                where.append('(\"{}\" = \'{}\')'.format(whereNames[n], whereValues[n]))
+            result = '''UPDATE {} SET
+                '''.format(table) + ''',
+                '''.join(result) + '''
+                WHERE''' + '''
+                AND '''.join(where) + '''
+                ;'''
+            return result
+
+        elif query = 'get columns':
+            result = '''SELECT column_name
+                FROM information_schema.columns
+                WHERE table_schema = \'{}\'
+                AND table_name   = \'{}\'
+                ;'''.format(schema_name.lower(), table)
+            return result
+
+        elif query = 'insert into':
+            result = '''
+                INSERT INTO \"{}\" (
+                    \"{}\")
+                VALUES (
+                    {})
+                ;'''.format(table, '''\",
+                    \"'''.join(listColumns), ''',
+                    '''.join(listValues)
+                    )
+            return result
+
+        else:
+            raise Exception('unknown option: \'{}\''.format(query))
+
     def newRow(self, schema_name = 'public', table_name = None, listNames = [], listValues = [], getID = True):
         ## check variables
         if table_name is None:
@@ -83,6 +196,7 @@ class database:
         if not isinstance(getID, bool):
             raise Exception('getID is type {}. Only boolean is allowed.'.format(type(getID)))
 
+        id = None
         names = []
         values = []
         for i in range( len(listValues) ):
@@ -91,7 +205,7 @@ class database:
                 values.append(listValues[i])
         listNames = names
         listValues = values
-        
+
         ## variables
         table = table_name.lower()
         schema = schema_name.lower()
@@ -99,23 +213,6 @@ class database:
         for i in range( len(listNames) ):
             names.append(listNames[i].lower())
         values = []
-
-        ## contruct query to read the ID
-        def getIDquery(table = None, rangeName = [], rangeValues = [], mode = None):
-            IDquery = []
-            for n in range( len(names) ):
-                if mode.lower() is 'check':
-                    IDquery.append('(\"{}\" = \'{}\')'.format(names[n], listValues[n]))
-                elif mode.lower() is 'update':
-                    IDquery.append('((\"{}\" = \'{}\') OR (\"{}\" IS NULL))'.format(names[n], listValues[n]), names[n])
-                else:
-                    return Exception('please choose mode for getID query: \'update\' or \'check\'.')
-            IDquery = '''SELECT MAX(id)
-                FROM \"''' + table + '''\"
-                WHERE ''' + '''
-                AND '''.join(IDquery) + '''
-                ;'''
-            return IDquery
 
         try:
             ## connect to DB
@@ -144,12 +241,22 @@ class database:
             # print(getIDquery)
             cursor.execute(getIDquery(table = table, rangeName = names, rangeValues = listValues, mode = 'update'))
             query = cursor.fetchall()
+            id = query[0][0]
             # print(query, query[0], type(query[0]), query[0][0], type(query[0][0]))
 
-            if query[0][0] is not None:
+            if id is not None:
                 ## update row
+                query = []
+                for n in range( len(names) ):
+                    query.append('(\"{}\" = \'{}\')'.format(names[n], listValues[n]))
+                query = '''UPDATE {} SET
+                    '''.format(table) + ''',
+                    '''.join(query) + '''
+                    WHERE id = {};'''.format(str(id))
+                cursor.execute(query)
+                connection.commit()
                 if getID:
-                    return query[0][0]
+                    return id
                 if not getID:
                     return
 
@@ -212,7 +319,7 @@ class database:
             ## get ID (if required)
             if getID:
                 # print(getIDquery)
-                cursor.execute(getIDquery)
+                cursor.execute(getIDquery(table = table, rangeName = names, rangeValues = listValues, mode = 'check'))
                 return cursor.fetchall()[0][0]
 
 
