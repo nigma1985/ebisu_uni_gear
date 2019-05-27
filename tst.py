3c58ff71a1ac363b88b6348d67e01a039ab044b6
nigma1985 <nigma-mania@web.de>
2019-05-27 08:30:54 +0200

    plotly tests

--- tst.py
+++ tst.py
@@ -1,43 +1,59 @@
-# import glob, os
-# # import module.read.pi as rpi
-# # from db import database
-# from module import json2py, removeFile
-# from module.connectPostgreSQL import database
-# from module.import_moves import move2sql
-#
-# os.chdir("../ebisu_uni_gear/")
-#
-# # ebisu = database(db_type=None, host='copyright', user='pi', password='21255Dohren', dbname='test')
-# # ebisu.newRow(table_name = 'myTest', listNames = ['driver', 'UsER'], listValues = ['Dummy', 'Konrad'])
-#
-# ebisu = database(db_type=None, host='copyright', user='pi', password='21255Dohren', dbname='test')
-#
-# x = ebisu.getSQL('''SELECT id, CAST ( duration AS FLOAT ) AS duration FROM activities;''')
-#
-# print(x)
-from datetime import datetime
-from datetime import timedelta
-import time
+import glob, os
 
-# nowsecs = time.mktime(datetime.now().timetuple())
-# print(nowsecs, type(nowsecs))
+from IPython.display import display, HTML
+import pandas as pd
+import numpy as np
+import math
 
-# def afunc(x):
-#     return x ** 2, 2 ** x
-#
-# a, b = afunc(10)
-#
-# c = afunc(5)
-#
-# print(a, b)
-# print(c)
+# Using plotly + cufflinks in offline mode
 
-#
-# def ret_dict(x):
-#     return {'x^2': x ** 2, '2^x': 2 ** x}
-#
-# print(ret_dict(11))
+import plotly.offline as py
+# import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import cufflinks as cf
+cf.set_config_file(offline=True)
 
-printout = ">>", 123, "<<"
 
-print("{} - {} - {}".format(printout))
+from module.connectPostgreSQL import database
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://postgres:21255Dohren@localhost:5434/postgres')
+df = pd.read_sql_query('SELECT * FROM public.v_superstore;',con=engine)
+
+
+os.chdir("../ebisu_uni_gear/")
+
+# for i in df:
+#     print(i)
+#
+# tab_x = pd.pivot_table(df,
+#     index='subcategory',
+#     columns='segment',
+#     values="profit",
+#     aggfunc=[np.sum, lambda x: len(x.dropna()) ])
+#
+# print(tab_x)
+#
+# for i in tab_x:
+#     print(i, tab_x[i])
+
+# old_df = pd.DataFrame(
+#     #data = ((1,3,5,7,9), (2,4,6,8,0)),
+#     #index = ('A', 'B', 'C', 'D', 'E'),
+#     # columns = ('odd', 'even')
+#     )
+old_df = {
+    'odd': (1,3,5,7,9),
+    'even': (2,4,6,8,0)
+}
+print(old_df)
+
+old_df = pd.DataFrame(
+    data = old_df,
+    index = ('A', 'B', 'C', 'D', 'E')#,
+    # columns = ('odd', 'even')
+    )
+
+print(old_df)
--- tst_superstore02.py
+++ tst_superstore02.py
@@ -54,15 +54,101 @@ py.plot(data, filename='plotly/cat_profits-basic-bar.html', auto_open=True)
 
 ################################################################################
 
-dt_profits = df.groupby(by='orderdate').sum()["profit"]
+# cat_profits = df.groupby(by=['category','segment']).sum()["profit"]
+# cat_profits = df.pivot(index='category', columns='segment', values="profit")
+cat_profits = pd.pivot_table(df,
+    index='category',
+    columns='segment',
+    values="profit",
+    aggfunc=np.sum)
+cat_cnts = pd.pivot_table(df,
+    index='category',
+    columns='segment',
+    values="profit",
+    aggfunc=lambda x: len(x.dropna()))
+
+op_max = 0
+for cat in cat_cnts:
+    for val in cat_cnts[cat]:
+        op_max = max(op_max, val)
+
+cat_opacity = cat_cnts
+for cat in cat_cnts:
+    cat_opacity[cat] = cat_opacity[cat] / op_max
+
+# print(cat_cnts, op_max, cat_opacity)
+
+# for x in cat_cnts:
+#     for y in cat_cnts[x]:
+#         # print(y, max(x[y]))
+#         print(y)
+#     print(x, cat_cnts[x], max(cat_cnts[x]))
+# cat_profits = cat_profits.sort_values(ascending=False)
+##################################################################################
+data = [
+    go.Bar(
+        x=cat_profits.index.values, # assign x as the dataframe column 'x'
+        y=cat_profits[col],
+        name = col,
+        marker=dict(
+            opacity=cat_opacity[col]
+            )
+    ) for col in cat_profits.columns
+]
+
+layout = go.Layout(
+    barmode='stack',
+    title='Stacked Bar with Pandas'
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+url = py.plot(fig, filename='plotly/cat_profits-segemnts-bar.html', auto_open=True)
+# py.plot(data, filename='plotly/cat_profits-segemnts-bar.html', auto_open=True)
+
+################################################################################
+
+# dt_profits = df.groupby(by='orderdate').sum()["profit"]
+
+dt_profits = pd.pivot_table(df,
+    index='orderdate',
+    columns='segment',
+    values="profit",
+    aggfunc=np.sum)
 dt_profits = dt_profits.sort_index(ascending=True)
+dt_cnts = pd.pivot_table(df,
+    index='orderdate',
+    columns='segment',
+    values="profit",
+    aggfunc=lambda x: len(x.dropna()))
+dt_cnts = dt_cnts.sort_index(ascending=True)
 
-print(dt_profits.index.values)
+op_max = 0
+for cat in dt_cnts:
+    for val in dt_cnts[cat]:
+        op_max = max(op_max, val)
 
-data = [go.Scatter(
-            x=dt_profits.index.values,
-            y=dt_profits,
-            mode='lines+markers'
-    )]
+dt_opacity = dt_cnts
+for cat in dt_cnts:
+    dt_opacity[cat] = dt_opacity[cat] / op_max # if dt_opacity[cat] is not pd.np.nan else 0
+
+dt_opacity = dt_opacity.replace({pd.np.nan: 0})
+
+# print(dt_opacity)
+#
+# print(dt_profits.index.values)
+
+data = [
+    go.Scatter(
+        x=dt_profits.index.values[0:100], # assign x as the dataframe column 'x'
+        y=dt_profits[col],
+        mode='lines',
+        # mode='marker',
+        name = col,
+        line=dict(
+            opacity=dt_opacity[col]
+            )
+    ) for col in dt_profits.columns
+]
 
 py.plot(data, filename='plotly/dt_profits-lines+markers.html', auto_open=True)
