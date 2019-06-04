9773a3096bb7b2943a3eadb7715b2d26d207aa6f
nigma1985 <nigma-mania@web.de>
2019-06-04 08:44:41 +0200

    timesplit poc

---
+++ timesplit.py
@@ -0,0 +1,17 @@
+# # aim:
+#
+# a time range from a to b with value x should be split into intervals
+# each intervall should hold a fraction of x
+#
+# example:
+# # data:
+# a = 2019-01-01 21:00:00
+# b = 2019-01-03 06:00:00
+# x = 66
+#
+# # solution:
+# intervalls = [
+#     (2019-01-01,  6),
+#     (2019-01-02, 48),
+#     (2019-01-03, 12)
+# ]
