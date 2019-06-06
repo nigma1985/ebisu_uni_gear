d2ffdd4c7dd5ee22d8aad92a6e8adf3b8606c214
nigma1985 <nigma-mania@web.de>
2019-06-06 09:08:00 +0200

    while added

--- timesplit.py
+++ timesplit.py
@@ -9,9 +9,9 @@ import pandas as pd
 # # data:
 
 
-a = datetime(2019, 1, 1, 21, 0, 0, 1) #2019-01-01 21:00:00
-b = datetime(2019, 1, 3, 6, 0, 0, 2) #2019-01-03 06:00:00
-x = 66
+start = datetime(2019, 1, 1, 21, 30, 0, 0) #2019-01-01 21:00:00
+end = datetime(2019, 1, 3, 6, 30, 0, 0) #2019-01-03 06:00:00
+val = 66
 
 # # solution:
 intervalls = [
@@ -20,9 +20,29 @@ intervalls = [
     (datetime(2019, 1, 3), 12)
 ]
 
-print(a, b, x)
+print(start, end, val)
 print(intervalls)
 
 ############################################################################
 
-print((b-a).total_seconds() / x)
+a = min(start, end)
+b = max(start, end)
+x = val
+
+span = (b - a).total_seconds()
+xp = x / span
+print(span, xp)
+
+
+cur_time = a.replace(microsecond=0,second=0,minute=0)
+times = []
+vals = []
+print(cur_time, times, vals)
+
+while cur_time < b.replace(microsecond=0,second=0,minute=0,hour=0)+timedelta(hours=1):
+    times.append(cur_time)
+    # vals.append()
+    cur_time = cur_time + timedelta(hours=1)
+    print(cur_time)
+
+print(times, vals)
