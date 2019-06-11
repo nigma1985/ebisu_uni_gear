1fb4d507cfa29da552c268e0453a7edd711ccd70
nigma1985 <nigma-mania@web.de>
2019-06-11 19:20:49 +0200

    new class: in/out data 2 json

--- -input_user.py
+++ -input_user.py
@@ -7,32 +7,45 @@ from module import json2py, py2json
 os.chdir("../ebisu_uni_gear/")
 
 
+class details:
+    def read(self, file):
+        if file is None:
+            raise Exception("missing file")
+        else:
+            pass
 
-data = []
-resume = file.replace(".zip", ".json").replace(".ZIP", ".json")
-try:
-    data = json2py(resume)
-except:
-    data['password number'] = p
-    data['password'] = None
-    data['file'] = file
-
-class unzip:
-    def getDir(self, path):
-        if path is None:
-            return
-        return Path(path).parent.absolute()
+        try:
+            print('file {} opened'.format(file))
+            return json2py(file)
+        except:
+            print('no file opened')
+            return []
+        finally:
+            pass
 
     # Initializer / Instance Attributes
     def __init__(self,
-        zipfile = None,
-        start = time.mktime(datetime.now().timetuple()),
-        password = None,
-        passnum = None,
-        passnum0 = 0):
+        json = '../details.json'
+        ):
 
-        self.zipfile = None
-        if zipfile is not None:
-            self.zipfile = zipfile
+        self.json = None
+        if json is not None:
+            self.json = json
         else:
-            raise
+            raise Exception("missing file")
+
+        self.details = self.read(self.json)
+
+    def write(self, data, file):
+        if data is None:
+            data = self.details
+        else:
+            pass
+
+        if file is None:
+            file = self.json
+        else:
+            pass
+
+        print('data written to', file)
+        py2json(data, file)
