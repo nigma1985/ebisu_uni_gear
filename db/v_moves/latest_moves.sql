f02483338e7935defd23f43d1073444ecd012ed2
nigma1985 <nigma-mania@web.de>
2019-06-16 23:47:50 +0200

    new querie for latest_moves

--- db/v_moves/latest_moves.sql
+++ db/v_moves/latest_moves.sql
@@ -1,29 +1,30 @@
--- first step towards standard view
-
-SELECT ma.id
-FROM public.v_moves AS ma
-LEFT JOIN
-	(SELECT
-		"user" AS "user", date AS date, MAX(lastupdate) AS lastupdate
-	FROM public.v_moves
-	GROUP BY "user", date) AS mb ON ma.date = mb.date AND ma."user" = mb."user"
-WHERE	ma.lastupdate = mb.lastupdate
-;
-
-
-------------------
-
-SELECT vact.*
-FROM v_activities AS vact
-LEFT JOIN v_segments AS vseg ON vseg.id = vact.segments
-LEFT JOIN v_moves AS vmov ON vmov.id = vseg.moves
-WHERE act.segments IN (
-	SELECT seg.id
-	FROM v_segments AS seg
-	WHERE seg.moves IN (
-		SELECT movb.id
-		FROM v_moves AS mova
-		JOIN v_moves AS movb ON (mova."user" = movb."user") AND (mova.date = movb.date)
-		GROUP BY mova."user", mova.date
-		)
-);
+SELECT
+-- 	mb.id,
+-- 	mb.date,
+	aa.id,
+	aa.activity,
+	aa.group,
+	aa.segments,
+	aa.manual,
+	aa.starttime,
+	aa.endtime,
+	aa.duration,
+	aa.steps,
+	aa.distance,
+	aa.calories,
+	mb.lastupdate,
+	mb.user
+FROM v_moves AS mb
+LEFT JOIN (
+	SELECT
+		MAX(lastupdate) AS lastupdate,
+		date,
+		"user"
+	FROM
+		v_moves AS ma
+	GROUP BY
+		date,
+		"user"
+	) AS mc USING (lastupdate, "user", date)
+LEFT JOIN v_segments AS sa ON sa.moves = mb.id
+LEFT JOIN v_activities AS aa ON aa.segments = sa.id
