2e23ffc77b626421535d4c143b3528139b0a1dc9
nigma1985 <nigma-mania@web.de>
2019-06-18 18:02:08 +0200

    class tankerkönig

--- _tankerkoenig.py
+++ _tankerkoenig.py
@@ -1,6 +1,96 @@
 import urllib.request, json
 from module import json2py, py2json
 
+class tankerkoenig:
+    def __init__(self,
+        dictionary = {},
+        lat = None,
+        lng = None,
+        rad = None,
+        sort = 'dist',
+        type = 'all',
+        apikey = None
+        ):
+        self.dictionary = None
+        if any(
+            len(dictionary) < 1,
+            dictionary['provider'] not in ('tankerkoenig.de', 'Tankerkönig'),
+            not isinstance(dictionary, dict),
+            dictionary is None
+        ):
+            raise Exception('dictionary missing!')
+        else:
+            self.dictionary = dictionary
+
+
+        self.lat = None  ## default value
+        if lat is not None:
+            self.lat = lat
+        else:
+            try:
+                self.lat = self.dictionary['lat']
+            exception:
+                pass
+
+        self.lng = None  ## default value
+        if lng is not None:
+            self.lng = lng
+        else:
+            try:
+                self.lng = self.dictionary['lng']
+            exception:
+                pass
+
+        self.rad = None
+        if lng is not None:
+            self.rad = rad
+        else:
+            try:
+                self.rad = self.dictionary['rad']
+            exception:
+                self.rad = 5.0 ## default value
+
+        self.sort = None
+        if sort is not None:
+            self.sort = sort
+        else:
+            try:
+                self.sort = self.dictionary['sort']
+            exception:
+                self.sort = 'dist' ## default value
+
+        self.type = None
+        if type is not None:
+            self.type = type
+        else:
+            try:
+                self.type = self.dictionary['type']
+            exception:
+                self.type = 'all' ## default value
+
+
+        self.type = None
+        if type is not None:
+            self.type = type
+        else:
+            try:
+                self.type = self.dictionary['type']
+            exception:
+                self.type = 'all'  ## default value
+
+        self.apikey = None ## default value
+        if apikey is not None:
+            self.apikey = apikey
+        else:
+            try:
+                self.apikey = self.dictionary['apikey']
+            exception:
+                try:
+                    self.apikey = self.dictionary['key']
+                exception:
+                    pass
+
+
 details = '../details.json'
 details = json2py(details)
 
@@ -23,7 +113,8 @@ print(call['key'])
 # sort = 'dist'
 # type = all
 apikey = call['key']
-#
+
+
 # url = "https://creativecommons.tankerkoenig.de/json/list.php?lat={}&lng={}&rad={}&sort={}&type={}&apikey={}".format(lat, lng, rad, sort, type, apikey)
 #
 # with urllib.request.urlopen(url) as site:
