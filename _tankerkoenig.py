c808e96d24a3eae0194e05511f096a607d5d5577
nigma1985 <nigma-mania@web.de>
2019-06-21 18:04:36 +0200

    bug fixing

--- _tankerkoenig.py
+++ _tankerkoenig.py
@@ -2,16 +2,21 @@ import urllib.request, json
 from module import json2py, py2json
 
 class tankerkoenig_api:
+    # see documentation on:
+    # https://creativecommons.tankerkoenig.de/
     def getVar(self,
         default = None,
         dictKey = None,
         value = None,
         theDict = {}):
-        if any(
-            theDict is None,
-            not isinstance(dictKey, dict),
-            len(dictKey) < 1):
-            raise Exception("dict missing")
+        if theDict is None:
+            raise Exception("dictionary missing")
+        elif not isinstance(theDict, dict):
+            print(theDict, type(theDict), len(theDict))
+            raise Exception("dictionary is no dictionary")
+        elif len(theDict) < 1:
+            raise Exception("dictionary empty")
+
         elif value is not None:
             return value
         elif isinstance(dictKey, (list, tuple)):
@@ -20,7 +25,7 @@ class tankerkoenig_api:
                     return theDict[entry]
                 except:
                     pass
-        elif isinstance(dictKey, (number, str)):
+        elif isinstance(dictKey, (int, float, str)):
             try:
                 return theDict[dictKey]
             except:
@@ -38,13 +43,14 @@ class tankerkoenig_api:
         apikey = None
         ):
         self.dictionary = None
-        if any(
-            len(dictionary) < 1,
-            dictionary['provider'] not in ('tankerkoenig.de', 'Tankerkönig'),
-            not isinstance(dictionary, dict),
-            dictionary is None
-        ):
+        if dictionary is None:
             raise Exception('dictionary missing!')
+        elif not isinstance(dictionary, dict):
+            raise Exception('dictionary is no dictionary!')
+        elif len(dictionary) < 1:
+            raise Exception('dictionary empty!')
+        elif dictionary['provider'] not in ('tankerkoenig.de', 'Tankerkönig'):
+            raise Exception('that\'s not my dictionary')
         else:
             self.dictionary = dictionary
 
@@ -79,6 +85,31 @@ class tankerkoenig_api:
             value = apikey,
             theDict = self.dictionary)
 
+        self.url = 'https://creativecommons.tankerkoenig.de'
+        self.format = 'json'
+
+    def call(self,
+        url = None,
+        format = None,
+        file = None,
+        options = {}
+        ):
+        if url is None:
+            url = self.url
+        else:
+            pass
+
+        if format is None:
+            format = self.format
+        else:
+            pass
+
+        if options is None or len(options) < 1:
+            return '{}/{}/{}'.format(url, format, file)
+        elif isinstance(options, dict):
+            return '{}/{}/{}?{}'.format(url, format, file, '&'.join([o + '=' + options[o] for o in options]))   ########
+
+
     def prt_all(self):
         print(self.lat,
             self.lng,
@@ -87,6 +118,57 @@ class tankerkoenig_api:
             self.type,
             self.apikey)
         print(self.dictionary)
+
+    def get_list(self,
+        lat = None,
+        lng = None,
+        rad = None,
+        sort = None,
+        type = None,
+        apikey = None,
+        dictionary = {}):
+
+        norm_dict = {
+            'lat' : self.getVar(
+                default = self.lat,
+                dictKey = 'lat',
+                value = lat,
+                theDict = dictionary),
+            'lng' : self.getVar(
+                default = self.lng,
+                dictKey = 'lng',
+                value = lng,
+                theDict = dictionary),
+            'rad' : self.getVar(
+                default = self.rad,
+                dictKey = 'rad',
+                value = rad,
+                theDict = dictionary),
+            'sort' : self.getVar(
+                default = self.sort,
+                dictKey = 'sort',
+                value = sort,
+                theDict = dictionary),
+            'type' : self.getVar(
+                default = self.type,
+                dictKey = 'type',
+                value = type,
+                theDict = dictionary),
+            'apikey' : self.getVar(
+                default = self.apikey,
+                dictKey = ['apikey', 'key'],
+                value = apikey,
+                theDict = dictionary)
+            }
+
+        return norm_dict
+
+    def get_prices(self):
+        pass
+    def get_detail(self):
+        pass
+    def get_complaint(self):
+        pass
 
 details = '../details.json'
 details = json2py(details)
@@ -121,3 +203,4 @@ apikey = call['key']
 
 tk = tankerkoenig_api(rad = 2.5, dictionary = call)
 tk.prt_all()
+print(tk.get_list())
