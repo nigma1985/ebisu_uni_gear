ec5faaf8c8413244860e429c6fed4ae60296348a
nigma1985 <nigma-mania@web.de>
2019-07-07 22:48:23 +0200

    sql connection for tankerkoenig

--- _tst.py
+++ _tst.py
@@ -1,3 +1,25 @@
-from random import random
+# import urllib.request, json
+from module import json2py, py2json
+from random import sample, random
+from module.connectPostgreSQL import database
 
-print(random())
+
+details = '../details.json'
+details = json2py(details)
+
+print('details', details)
+postgres = []
+for d in details:
+    if d['provider'] == 'PostgreSQL' and d['host'] == 'localhost':
+        postgres.append(d)
+    else:
+        pass
+
+if len(postgres) > 0:
+    postgres = postgres[0]
+else:
+    pass
+
+print(postgres, postgres['host'], postgres['password'])
+
+ebisu = database(db_type=None, port=postgres['port'], host=postgres['host'], user=postgres['user'], password=postgres['password'], dbname=postgres['database'])
