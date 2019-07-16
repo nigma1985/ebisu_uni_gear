366b4dbc1011e4dbf598992628e952e130e4641f
nigma1985 <nigma-mania@web.de>
2019-07-16 09:12:29 +0200

    date/string convertion

--- _move_anonymize.py
+++ _move_anonymize.py
@@ -1,4 +1,4 @@
-import glob, os, sys
+import glob, os, sys, re
 from zipfile import ZipFile
 from pathlib import Path
 
@@ -13,13 +13,13 @@ from random import sample, randint
 start_time = time.mktime(datetime.now().timetuple())
 
 # os.chdir("/home/pi/ebisu_uni_gear/")
-os.chdir("../ebisu_uni_ge/")
+os.chdir("../ebisu_uni_gear/")
 
 def neoEntry(entry):
-    if 'x' in entry: ##if date
-        return 'entry'
-    elif 'x' in entry: ##if datetime
-        return 'entry'
+    if len(entry) in (16,20) and re.search("(((19|20)([2468][048]|[13579][26]|0[48])|2000)0229|((19|20)[0-9]{2}(0[469]|11)(0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}(0[13578]|1[02])(0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}02(0[1-9]|1[0-9]|2[0-8])))", entry): ##if date
+        return datetime.strptime(entry, '%Y%m%dT%H%M%S%z').strftime("%Y-%m-%d %H:%M:%S%z")
+    elif len(entry) == 8 and re.search("(((19|20)([2468][048]|[13579][26]|0[48])|2000)0229|((19|20)[0-9]{2}(0[469]|11)(0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}(0[13578]|1[02])(0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}02(0[1-9]|1[0-9]|2[0-8])))", entry): ##if date
+            return datetime.strptime(entry, '%Y%m%d').strftime("%Y-%m-%d")
     elif 'x' in entry: ##if datetime TZ
         return 'entry'
     elif 'x' in entry: ##if Aileen
