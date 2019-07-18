5ada62151820e83b8cc5ead2e1aaa4d4e7c246f7
nigma1985 <nigma-mania@web.de>
2019-07-18 18:26:46 +0200

    v_tankerkoenig_stations

--- db/v_tankerkoenig_stations.sql
+++ db/v_tankerkoenig_stations.sql
@@ -1,13 +1,11 @@
-DROP VIEW IF EXISTS v_coordinates CASCADE;
+DROP VIEW IF EXISTS v_tankerkoenig_stations CASCADE;
 
-CREATE VIEW v_coordinates AS
+CREATE VIEW v_tankerkoenig_stations AS
 (SELECT
-	loca.lat AS lat,
-	loca.lng AS lng,
+	loca.station_location_id AS station_location_id,
 	COUNT(DISTINCT res.tankerkoenig) AS n,
  	MAX(tk.utc_datetime) AS utc_datetime,
- 	tk."user" AS "user",
-	'tankerkoenig' AS "source"
+ 	tk."user" AS "user"
 FROM
 	public.results AS res
 LEFT JOIN
@@ -17,8 +15,7 @@ LEFT JOIN
 WHERE
 	loca.lat IS NOT NULL AND loca.lng IS NOT NULL
 GROUP BY
-	loca.lat,
-	loca.lng,
+	loca.station_location_id,
 	res.station_location,
  	tk."user"
 );
