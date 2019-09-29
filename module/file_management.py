d0dd7f1a845bdf1addcda0db393aabcf591cb6c8
nigma1985 <nigma-mania@web.de>
2019-09-29 23:11:15 +0200

    model & make...

--- module/file_management.py
+++ module/file_management.py
@@ -1,4 +1,5 @@
-import glob, os
+import glob, os, re, exifread
+sep = os.path.sep
 
 def get_files(path = ""):
     if path != "":
@@ -12,13 +13,44 @@ def is_file(element = ""):
     if element != "":
         return "." in element
 
+def is_file_type(element = None, type = None, regex = None):
+    if element is None or (type is None and regex is None):
+        return None
+    elif regex is None and type is not None:
+        regex = "\\{}$".format(type)
+    else:
+        pass
+    return re.search(regex, element, re.IGNORECASE)
+
+def read_tags(path_name = None):
+    f = open(path_name, 'rb')
+    return exifread.process_file(f, details=True)
+
+def key_value(dictionary = {}, key = None):
+    try:
+        return dictionary[key]
+    except:
+        return None
+
+def dt_min_max(dictionary = {}, key = "Date"):
+    result = [(d, dictionary[d]) for d in dictionary if key in dictionary]
+    return (result, result)
+
+###############################################################################
+###############################################################################
 
 class file:
     def __init__(self, directory = None, element = None, types = []):
         self.directory = directory
         self.element = element
+        self.file_path = directory + sep + element
 
-        # self.file_path = directory + "\\" + element
+        self.tags = None
+        self.model = None
+        self.make = None
+        self.dt_min = None
+        self.mt_max = None
+        # self.model = None
 
         if is_folder(element = element):
             self.type = "folder"
@@ -26,17 +58,29 @@ class file:
         elif types is None:
             self.type = None
             self.tid = None
+            # self.tags = read_tags(path_name = self.file_path)
         # elif isinstance(types, [list, tuple]) and len(types) > 0:
         #     self.type = None
         else:
             self.type = "other"
             self.tid = 0
             for type in types:
-                if type in element:
+                # if type in element:
+                if is_file_type(element = element, type = type):
                     self.type = type
                     self.tid = 1
+                    self.tags = read_tags(path_name = self.file_path)
                 else:
                     pass
+
+        if self.tags is None:
+            pass
+        elif len(self.tags) == 0:
+            self.tags = None
+        else:
+            self.model = key_value(dictionary = self.tags, key = "Image Model")
+            self.make = key_value(dictionary = self.tags, key = "Image Make")
+            self.dt_min, self.dt_max = dt_min_max(dictionary = self.tags)
 
     def move(self, orig, dest):
         print(self.element, orig, dest, self.tid, self.type)
@@ -54,9 +98,17 @@ class file:
         else:
             pass
 
+###############################################################################
+###############################################################################
 
 class drcty:
-    def __init__(self, orig = None, dest = None, types = []):
+    def type2regex(self, regex = None, types = None):
+        if regex is None or len(regex) != len(types):
+            return ["\\{}$".format(type) for type in types]
+        else:
+            return regex
+
+    def __init__(self, orig = None, dest = None, types = [], regex = []):
         if drcty is None:
             self.orig = os.getcwd()
         else:
@@ -70,6 +122,7 @@ class drcty:
         self.files = get_files(self.orig)
 
         self.types = types
+        self.regex = self.type2regex(regex = regex, types = self.types)
 
     def get_types(self):
         lst = []
@@ -82,7 +135,14 @@ class drcty:
         lst = []
         for elem in self.files:
             f = file(directory = self.orig, element = elem, types = self.types)
-            lst.append(f.type)
+            lst.append(f.tid)
+        return lst
+
+    def get_tags(self):
+        lst = []
+        for elem in self.files:
+            f = file(directory = self.orig, element = elem, types = self.types)
+            lst.append(f.tags)
         return lst
 
     def move_one(self, orig, elem, dest):
