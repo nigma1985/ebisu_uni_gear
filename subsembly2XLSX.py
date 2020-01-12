3d2d24739c673cbb8105a0ef0f4c22102ba296ca
nigma1985 <nigma-mania@web.de>
2020-01-12 20:40:10 +0100

    fixxing naming issues

--- subsembly2XLSX.py
+++ subsembly2XLSX.py
@@ -1,54 +1,39 @@
-import os
-import json
+import glob, os, openpyxl
+from module import json2py, py2json
+import numpy as np
+import pandas as pd
 
-os.chdir("D:/OneDrive/Dokumente/moves_20180731/json")
+base_path = "C:\\Users\\Konrad\\OneDrive\\Dokumente\\kto"
+sep = os.path.sep
+ext = "20200111"
 
-print(os.getcwd())
-
-
-## jsonStoryline = open("json/full/storyline.json")
-with open('json/full/activities.json', 'r') as f:
-    jsonActivities = json.load(f)
-print(len(jsonActivities))
-print(jsonActivities[1])
+print(sep)
 
 
-# ## jsonSummary = open("json/full/summary.json")
-# with open('json/full/places.json', 'r') as f:
-#     jsonPlaces = json.load(f)
-# print(len(jsonPlaces))
-# print(jsonPlaces[1])
+files = (
+    "539947056",
+    "2204012900",
+    "2204012920",
+    "2204012950",
+    "2204012955")
 
+jsn = []
+keys = set()
 
-# ## jsonPlaces = open("json/full/places.json")
-# with open('json/full/storyline.json', 'r') as f:
-#     jsonStoryline = json.load(f)
-# print(len(jsonStoryline))
-# print(jsonStoryline[1])
+for file in files:
+    result = json2py("{}{}{}-{}.json".format(base_path, sep, file, ext))
+    # print(type(result), result)
+    for item in result:
+        item['ACCOUNT'] = file
+        jsn.append(item)
+        for key in item:
+            keys.add(key)
 
+# for x in jsn:
+#     print(x)
 
-## jsonActivities =open("json/full/activities.json")
-# with open('json/full/summary.json', 'r') as f:
-#     jsonSummary = json.load(f)
-# print(len(jsonSummary))
-# print(jsonSummary[1])
+print(keys)
 
-lineOne = jsonActivities[6]
-print(lineOne['date'])
-print(lineOne['summary'])
-print(len(lineOne['summary']))
-print(lineOne['segments'])
-print(len(lineOne['segments']))
-print(lineOne['caloriesIdle'])
-#print(len(lineOne['caloriesIdle']))
-print(lineOne['lastUpdate'])
-#print(len(lineOne['lastUpdate']))
-
-lineOneSummary = lineOne['summary']
-print(len(lineOneSummary))
-
-lineOneSegments = lineOne['segments']
-print(len(lineOneSegments))
-
-for seg in lineOneSegments:
-    print(seg)
+frm = pd.DataFrame(jsn)
+frm.to_excel("{}{}{}-{}.xlsx".format(base_path, sep, "output", ext))
+# print(frm)
